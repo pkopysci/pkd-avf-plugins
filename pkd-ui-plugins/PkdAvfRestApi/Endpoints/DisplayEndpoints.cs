d0dd0c9fd92814b33179301f2389248ad4805d03
Philip J Kopyscinski <phil.kopysci@gmail.com>
2025-04-02 12:50:21 -0700

    added testing display data transfer objects.

---
+++ pkd-ui-plugins/PkdAvfRestApi/Endpoints/DisplayEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using pkd_application_service;
+using PkdAvfRestApi.Contracts;
+
+namespace PkdAvfRestApi.Endpoints;
+
+public static class DisplayEndpoints
+{
+    private const string GetDisplayEndpointName = "GetDisplay";
+
+    private static readonly List<DisplayDto> Displays =
+    [
+        new(1, "Projector", "NEC", "ABC-123", true, true, true, []),
+        new(2, "Display 1", "NEC", "FP-55", true, false, true, []),
+        new(3, "Display 2", "NEC", "FP-55", false, false, true, ["station"])
+    ];
+
+    private static ApplicationService? _appService;
+
+    public static RouteGroupBuilder MapDisplayEndpoints(this WebApplication app, ApplicationService service)
+    {
+        _appService = service;
+        
+        var group = app.MapGroup("displays");
+        
+        group.MapGet("/", () => Displays)
+            .WithName("Displays")
+            .WithOpenApi();
+
+        return group;
+    }
+}
